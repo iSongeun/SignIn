961346135761a0f8f936b95d9f3fa72edaf30c8f
luckybsmith <vs1707@naver.com>
2023-03-10 16:56:22 +0900

    init

--- SingIn/ViewController.swift
+++ SingIn/ViewController.swift
@@ -9,11 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passTF: UITextField!
+    @IBOutlet weak var emailLB: UILabel!
+    @IBOutlet weak var passLB: UILabel!
+    
+    var emailErrorHeight : NSLayoutConstraint!
+    var passErrorHeight : NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        emailTF.addTarget(self, action: #selector(textfieldChanged), for: .editingChanged)
+        passTF.addTarget(self, action: #selector(textfieldChanged), for: .editingChanged)
+        
+        emailErrorHeight = emailLB.heightAnchor.constraint(equalToConstant: 0)
+        passErrorHeight = passLB.heightAnchor.constraint(equalToConstant: 0)
     }
 
+    @objc func textfieldChanged(textField : UITextField){
+        if textField == emailTF {
+            if isValidEmail(emailTF.text ?? "") {
+                emailErrorHeight.isActive = true
+            }
+            else {
+                emailErrorHeight.isActive = false
+            }
+        }
+        else if textField == passTF {
+            if isValidPassword(passTF.text ?? "") {
+                passErrorHeight.isActive = true
+            }
+            else {
+                passErrorHeight.isActive = false
+            }
+        }
+        
+        UIView.animate(withDuration: 0.2) {
+            self.view.layoutIfNeeded() //wow!!
+        }
+    }
+    
+    //정규식
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
 
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    
+    func isValidPassword (_ password : String) -> Bool {
+        if password.count < 8 {
+            return false
+        }
+        return true
+    }
 }
-
